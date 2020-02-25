4ba0cbd5159c250e3d5b0027b9d6eea0476e2d57
Ariel <wintersbastian@gmail.com>
2020-02-25 20:45:29 +0100

    Cambios en Factura.java (Se añadieron 4 variables de clase)

--- ED/src/practica/Facturas.java
+++ ED/src/practica/Facturas.java
@@ -2,7 +2,10 @@ package practica;
 
 public class Facturas 
 {
-
+	public int num;
+	public float base;
+	public float tipoIva;
+	// public Estados estado;
 	public static void main(String[] args) 
 	{
 		
