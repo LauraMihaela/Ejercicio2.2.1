413afc76708e9ee9faeb576e984818ef4be78a36
Ruba Mulla <ruba.mulla00@gmail.com>
2020-02-27 18:23:43 +0100

    Factura

--- ED/src/practica/Facturas.java
+++ ED/src/practica/Facturas.java
@@ -1,11 +1,29 @@
 package practica;
 
-public class Facturas 
+public class Facturas implements IFactura
 {
 
 	public static void main(String[] args) 
 	{
 		
+	}
+
+	@Override
+	public void cambiarEstado(Estados estado) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public float getTotal() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public float getCantidadIva() {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 
 }
